55d167f7d597b83954d0c0b6115a05f340980ca0
Dot In <57308042+D071N@users.noreply.github.com>
2022-12-06 15:03:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var adminSocketId=null;
 const port = 8080;
 
 server.listen(process.env.PORT || port, (err) => {  if (err) return;log("Server Started : " + port);});
-app.get('/', (req, res) => res.send('Server Is Running.....'))
+app.get('/', (req, res) => res.send('Server is Running.....'))
 
 io.on('connection', (socket) => {
     socket.on('adminJoin', ()=>{
